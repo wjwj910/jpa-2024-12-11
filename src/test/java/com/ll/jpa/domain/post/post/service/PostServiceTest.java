6c99b41143de9ffe4b5c63827ec94b67c52d2c58
wjwj910 <wjwj910@gmail.com>
2025-01-07 00:00:27 +0900

    work

--- src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +16,6 @@ class PostServiceTest {
 	@Test
 	@DisplayName("글 2개 생성")
 	@Transactional
-	@Rollback(false)
 	void t1() {
 		postService.write("title1", "content1");
 		postService.write("title2", "content2");

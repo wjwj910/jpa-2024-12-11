3f8f65e1cb58cadcfa124cd4aa3d552bd19742dd
wjwj910 <wjwj910@gmail.com>
2025-01-06 15:51:34 +0900

    work

--- src/main/java/com/ll/jpa/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jpa/global/initData/BaseInitData.java
@@ -38,8 +38,6 @@ public class BaseInitData {
                 PostComment postComment2 = postCommentService.write(post1, "comment2");
                 // 2번 글에 대한 댓글 1 생성
                 PostComment postComment3 = postCommentService.write(post2, "comment3");
-
-                Post postOfComment3 = postComment3.getPost();
             }
         };
     }
@@ -47,26 +45,26 @@ public class BaseInitData {
     @Bean
     @Order(2)
     public ApplicationRunner baseInitData2ApplicationRunner() {
-        return new ApplicationRunner() {
-            @Transactional
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                PostComment postComment3 = postCommentService.findById(3).get();
-                /*
-                SELECT PC.*
-                FROM post_comment AS PC
-                WHERE PC.id = 3
-                 */
-                Post postOfComment3 = postComment3.getPost();
-                System.out.println("postOfComment3.id = " + postOfComment3.getId());
-                System.out.println("postOfComment3.title = " + postOfComment3.getTitle());
-                /*
-                SELECT P.*
-                FROM post AS P
-                WHERE P.id = 2
-                */
-                System.out.println("postOfComment3.content = " + postOfComment3.getContent());
-            }
-        };
+        return args -> work();
+    }
+
+    @Transactional
+    public void work() {
+        PostComment postComment3 = postCommentService.findById(3).get();
+        /*
+        SELECT PC.*
+        FROM post_comment AS PC
+        WHERE PC.id = 3
+        */
+
+        Post postOfComment3 = postComment3.getPost();
+        System.out.println("postOfComment3.id = " + postOfComment3.getId());
+        System.out.println("postOfComment3.title = " + postOfComment3.getTitle());
+        /*
+        SELECT P.*
+        FROM post AS P
+        WHERE P.id = 2
+        */
+        System.out.println("postOfComment3.content = " + postOfComment3.getContent());
     }
 }
